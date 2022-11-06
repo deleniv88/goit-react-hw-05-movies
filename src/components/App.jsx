826110730a47d376e479c0c02305b2924445daf3
Andrew <deleniv08@gmail.com>
2022-11-06 15:59:11 +0100

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ export const App = () => {
           <Route path='reviews' element={<Reviews />} />
         </Route>
       </Route>
-      <Route path='*' element={<div>Error</div>} />
+      <Route path='*' element={<Home/>} />
     </Routes>
   )
 }
