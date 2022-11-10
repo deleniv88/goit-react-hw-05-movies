3773a291d4fe761ce4b2e113df3ce066a85322a6
Andrew <deleniv08@gmail.com>
2022-11-10 20:11:49 +0100

    new

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,17 +1,15 @@
 import { Container, NavItem } from "./AppBar.styled"
 
-// const navItems = [
-//   { href: '/', text: 'Home' },
-//   { href: 'movies', text: 'Movies' }
-// ]
+const navItems = [
+  { href: '/', text: 'Home' },
+  { href: 'movies', text: 'Movies' }
+]
 
 export const AppBar = () => {
   return (
     <Container>
-        <NavItem to='/'>Home</NavItem>
-        <NavItem to='movies'>Movies</NavItem>
-      {/* {navItems.map(({ href, text }) =>
-        <NavItem to={href} key={href}>{text}</NavItem>)} */}
+      {navItems.map(({ href, text }) =>
+        <NavItem to={href} key={href}>{text}</NavItem>)}
     </Container>
   )
 }
